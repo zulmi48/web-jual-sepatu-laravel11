2bc1e850349891bcf3a2366ae347ecf8ff109875
Zulmi F. <zulmi.f48@gmail.com>
2026-05-02 23:07:50 +0700

    setup controller order and front

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,72 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCustomerDataRequest;
+use App\Http\Requests\StoreOrderRequest;
+use App\Http\Requests\StorePaymentRequest;
+use App\Models\ProductTransaction;
+use App\Models\Shoe;
+use App\Services\OrderService;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    protected $orderService;
+
+    public function __construct(OrderService $orderService)
+    {
+        $this->orderService = $orderService;
+    }
+
+    public function saveOrder(StoreOrderRequest $request, Shoe $shoe)
+    {
+        $validated = $request->validated();
+        $validated['shoe'] = $shoe->id;
+        $this->orderService->beginOrder($validated);
+
+        return redirect()->route('front.booking', $shoe->slug);
+    }
+
+    public function booking()
+    {
+        $data = $this->orderService->getOrderDetails();
+        return view('order.order', $data);
+    }
+
+    public function customerData()
+    {
+        $data = $this->orderService->getOrderDetails();
+        return view('order.customer_data', $data);
+    }
+
+    public function saveCustomerData(StoreCustomerDataRequest $request)
+    {
+        $validated = $request->validated();
+        $this->orderService->updateCustomerData($validated);
+
+        return redirect()->route('front.payment');
+    }
+
+    public function payment()
+    {
+        $data = $this->orderService->getOrderDetails();
+        return view('order.payment', $data);
+    }
+
+    public function paymentConfirm(StorePaymentRequest $request)
+    {
+        $validated = $request->validated();
+        $productTransactionId = $this->orderService->paymentConfirm($validated);
+
+        if ($productTransactionId) {
+            return redirect()->route('front.order_finished', $productTransactionId);
+        }
+
+        return redirect()->route('front.index')->withErrors(['error' => 'Payment failed. please try again']);
+    }
+
+    public function orderFinished(ProductTransaction $productTransaction)
+    {
+        dd($productTransaction);
+    }
 }
---
+++ app/Http/Requests/StoreCustomerDataRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCustomerDataRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'phone' => ['required', 'string', 'max:255'],
+            'city' => ['required', 'string', 'max:255'],
+            'post_code' => ['required', 'string', 'max:255'],
+            'address' => ['required', 'string'],
+        ];
+    }
+}
